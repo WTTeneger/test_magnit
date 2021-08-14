a9e7125b41964c8788936b35791a653e37e7a73e
WTTeneger <50665935+WTTeneger@users.noreply.github.com>
2021-08-14 18:56:14 +0300

    ф

--- test2/__settings/env.py
+++ test2/__settings/env.py
@@ -6,6 +6,6 @@ JWT_REFRESH_SECRET = 'JWT_REF'
 DB_HOST = '37.140.192.90'
 DB_USER = 'u1102095_auction'
 DB_PASSWORD = 'auction!'
-DB_DATABASE = 'auction'
+DB_DATABASE = 'u1102095_auction'
 DB_CHARSET = 'utf8mb4'
 
